022e11c678d841663809f12485d0c7f03a3e0750
kathir-vel-in <kathir@proptelli.com>
2025-09-30 12:46:37 +0530

    PCF-2 header desktop UI complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="bg-greywhite shadow-sm w-full flex justify-between">
+      <div className="flex items-center pl-60">
+        <div className="pt-12">
+          <img
+            width={131}
+            height={45}
+            src={"src/assets/Proptelli_Logo.svg"}
+          />
+        </div>
+      </div>
+      <div className="flex items-center justify-between pt-28 pb-20 pr-74">
+        <div className="flex items-center gap-4">
+          <button className="p-2 bg-white border border-darkgreywhite hover:bg-gray-100 rounded-full w-50 h-50 flex justify-center transition-colors">
+            <img
+              width={24}
+              height={24}
+              src={"src/assets/icons/search_icon.svg"}
+            />
+          </button>
 
-export default Header
+          <button className="relative p-2 bg-white border border-darkgreywhite hover:bg-gray-100 w-50 h-50 flex justify-center items-center rounded-full transition-colors">
+             <img width={21} height={21} src={"src/assets/icons/notification_icon.svg"} alt="Notifications"/>
+            <span className="absolute -top-1 -right-1 bg-yellow-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
+              3
+            </span>
+          </button>
+
+          <button className="relative ">
+            <img
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop"
+              alt="Profile"
+              className="w-10 h-10 rounded-full w-[50px] h-[50px] flex justify-center object-cover border-4 border-gray-200 hover:border-yellow-500 transition-colors"
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -4,11 +4,13 @@ import Header from "../components/Header";
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
+    <div className="flex flex-col h-screen">
+      <Header />
 
-      <div className="flex-1 flex flex-col">
-        <div className=" flex pl-9"><Header /></div>
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+
+        {/* Main Content Area */}
         <main className="flex-1 p-6 bg-gray-50 overflow-auto">
           <Outlet />
         </main>
