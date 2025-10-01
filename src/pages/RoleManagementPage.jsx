79ab7b0a7c32fd6206c90153d19fe50ed61617fc
kathir-vel-in <kathir@proptelli.com>
2025-10-01 19:20:04 +0530

    PCF-20 permission card completed

--- src/pages/RoleManagementPage.jsx
+++ src/pages/RoleManagementPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import PermissionSection from '../components/PermissionSection';
 
 const RoleManagementPage = () => {
+  const initialPermissions = [
+    { label: "VIEW", active: true },
+    { label: "CREATE", active: false },
+    { label: "EDIT", active: false },
+    { label: "DELETE", active: false },
+    { label: "ASSIGN", active: false },
+  ];
   return (
     <div>
       Role Management 
+      <div className="p-6">
+      <PermissionSection initialPermissions={initialPermissions}/>
+    </div>
     </div>
   )
 }
